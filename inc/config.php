fe342a0db4c466f334fd4dd1b3e247f2dac13bf5
Allen Garvey <allengarvey@hotmail.com>
2016-01-24 19:47:54 -0500

    add views path constant

--- inc/config.php
+++ inc/config.php
@@ -6,6 +6,7 @@ define('APP_NAME', 'Bitter Polar Bear');
 //path constants
 define('ROOT_PATH', dirname(__FILE__, 2).'/');
 define('INC_PATH', ROOT_PATH.'inc/');
+define('VIEWS_PATH', INC_PATH.'views/');
 
 //login form constants
 define('LOGIN_FORM_USER', 'username');
