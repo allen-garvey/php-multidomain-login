51a85e731f8bd090f0943c75d656eba0583d310e
Allen Garvey <allengarvey@hotmail.com>
2016-01-24 20:13:35 -0500

    add basic styles inline for performance

--- inc/views/header.php
+++ inc/views/header.php
@@ -2,7 +2,39 @@
 <html>
 <head>
 	<title><?= APP_NAME ?> - Login</title>
-	<link rel="stylesheet" type="text/css" href="<?= STYLESHEETS_URL.'style.css'; ?>" />
+	<style>
+		html,body{
+			margin: 0;
+			font-family: Helvetica, Arial, sans-serif;
+		}
+		.header_title{
+			text-align: center;
+			margin: 0;
+		}
+		.container{
+			max-width: 40em;
+			margin: 12em auto 0;
+		}
+		.login_form{
+			background-color: #ccc;
+			padding: 3em;
+			border-radius: 5px;
+		}
+		input,button{
+			font-size: 1.2em;
+		}
+		input{
+			display: block;
+			width: 25em;
+			max-width: 100%;
+			padding: 0.25em 0.1em;
+			margin-bottom: 1em;
+		}
+		button{
+			float: right;
+			cursor: pointer;
+		}
+	</style>
 </head>
 <body>
 
