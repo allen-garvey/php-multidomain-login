47217ddd9fe015701ba99d4c620fb6cae699f4ee
Allen Garvey <allengarvey@hotmail.com>
2016-01-24 20:13:18 -0500

    add submit button, input labels, form container and header title

--- inc/views/login-form.php
+++ inc/views/login-form.php
@@ -1,15 +1,21 @@
 <div class="container">
-	<form method="POST" action="" class="login_form">
-		<div class="form-group">
-			<input type="text" name="<?= LOGIN_FORM_USER; ?>" />
-			<input type="password" name="<?= LOGIN_FORM_PASS; ?>" />
-		</div>
-		<div class="form-errors">
-			<?php 
-				if (isset($form_errors)) {
-					echo $form_errors;
-				}
-			?>
-		</div>
-	</form>
+	<div class="login_form">
+		<h1 class="header_title"><?= APP_NAME ?></h1>
+		<form method="POST" action="" class="login_form">
+			<div class="form-group">
+				<label for="<?= LOGIN_FORM_USER; ?>">Username</label>
+				<input type="text" placeholder="Username" id="<?= LOGIN_FORM_USER; ?>" name="<?= LOGIN_FORM_USER; ?>" autofocus="autofocus" />
+				<label for="<?= LOGIN_FORM_PASS; ?>">Password</label>
+				<input type="password" placeholder="Password" id="<?= LOGIN_FORM_PASS; ?>" name="<?= LOGIN_FORM_PASS; ?>" />
+				<button type="submit">Login</button>
+			</div>
+			<div class="form-errors">
+				<?php 
+					if (isset($form_errors)) {
+						echo $form_errors;
+					}
+				?>
+			</div>
+		</form>
+	</div>
 </div>
