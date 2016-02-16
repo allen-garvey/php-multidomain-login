e5233155f3c3857ef99bb7dbac9da3bbc727c9bf
Allen Garvey <allengarvey@hotmail.com>
2016-02-15 22:27:20 -0500

    add functions to destroy session and authenticate user

--- inc/functions.php
+++ inc/functions.php
@@ -2,16 +2,38 @@
 
 
 function destroy_session(){
-	$_SESSION[SESSION_AUTH_KEY] = null;
+	//remove PHPSESSID from browser
+	if ( isset( $_COOKIE[session_name()] ) ){
+		setcookie( session_name(), '', time()-3600, '/' );
+	}
+	//clear session from globals
+	$_SESSION = array();
+	//clear session from disk
+	session_destroy();
 }
 
-function is_authenticated($username, $password){
-	return true;
+function is_authenticated($username, $password): bool{
+	if(!empty($username) && !empty($password) && $username === MASTER_USER && $password === MASTER_PASS){
+		return true;
+	}
+	return false;
+}
+
+function authenticate(){
+	if(REQUIRE_LOGIN === false){
+		return;
+	}
+	session_start();
+	if(empty($_SESSION) || empty($_SESSION[SESSION_AUTH_KEY])){
+		http_response_code(400);
+		header('Location: '.LOGIN_URL);
+		die();
+	}
 }
 
 /*
 * Saves that the user is logged in in session so other subdomains can access it
 */
-function save_auth($username){
+function save_auth(string $username){
 	$_SESSION[SESSION_AUTH_KEY] = $username;
 }
