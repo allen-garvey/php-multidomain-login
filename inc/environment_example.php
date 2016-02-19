f7ef43cfee67c79f80836431c817cd56c847d8d7
Allen Garvey <allengarvey@hotmail.com>
2016-02-18 22:37:34 -0500

    rename example environment file and change master password to password hash

--- inc/environment_example.php
+++ inc/environment_example.php
@@ -5,4 +5,4 @@ define('ENV_CURRENT', ENV_DEV);
 //authentication
 define('REQUIRE_LOGIN', true); //set to false to disable authentication
 define('MASTER_USER', 'admin');
-define('MASTER_PASS', 'password');
+define('MASTER_PASSWORD_HASH', '$2y$10$ATf3bvLiH3d36SLUx2/IPup1VlpBBh6qk619BGkk6EVK7JcZ57dMO');
