3bc73cd5323922906b42a6b5d74a951482022555
Allen Garvey <allengarvey@hotmail.com>
2016-02-19 22:58:04 -0500

    add captcha constants

--- inc/login_config.php
+++ inc/login_config.php
@@ -6,11 +6,16 @@ define('LOGIN_VIEWS_PATH', VIEWS_PATH.'login/');
 //login form constants
 define('LOGIN_FORM_USER', 'username');
 define('LOGIN_FORM_PASS', 'password');
+define('LOGIN_FORM_CAPTCHA', 'captcha');
+
+//captcha constants
+define('CAPTCHA_NUMBER_MIN', 1);
+define('CAPTCHA_NUMBER_MAX', 10);
 
 //session constants
 define('SESSION_AUTH_KEY', 'session_username_key');
 define('SESSION_REDIRECT_URL_KEY', 'session_redirect_url_key');
-
+define('SESSION_CAPTCHA_ANSWER_KEY', 'session_captcha_answer_key');
 
 //urls
 define('LOGGED_IN_HOME_URL', BASE_URL.'index.php');
