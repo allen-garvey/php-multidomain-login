749c6b3a18d85030a290a797f43f5797c66b270e
Allen Garvey <allengarvey@hotmail.com>
2016-02-19 22:58:29 -0500

    add captcha to login form

--- inc/views/login/login-form.php
+++ inc/views/login/login-form.php
@@ -7,6 +7,8 @@
 				<input type="text" placeholder="Username" id="<?= LOGIN_FORM_USER; ?>" name="<?= LOGIN_FORM_USER; ?>" autofocus="autofocus" />
 				<label for="<?= LOGIN_FORM_PASS; ?>">Password</label>
 				<input type="password" placeholder="Password" id="<?= LOGIN_FORM_PASS; ?>" name="<?= LOGIN_FORM_PASS; ?>" />
+				<label for="<?= LOGIN_FORM_CAPTCHA; ?>"><?= $captcha_text; ?></label>
+				<input type="number" placeholder="?" id="<?= LOGIN_FORM_CAPTCHA; ?>" name="<?= LOGIN_FORM_CAPTCHA; ?>" />
 				<button type="submit">Login</button>
 			</div>
 			<div class="form-errors">
