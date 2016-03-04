70ab8e9a3c17c86402607c0f3c454de694894821
Allen Garvey <allengarvey@hotmail.com>
2016-03-04 12:04:58 -0500

    add logout link to test pages

--- public_html/index.php
+++ public_html/index.php
@@ -3,10 +3,8 @@
 require_once('../inc/config.php');
 authenticate();
 
+?>
 
+<a href="<?= LOGOUT_URL; ?>">Logout</a>
+<p>Hello there! It looks like you're logged in</p>
 
-include(LOGIN_VIEWS_PATH.'header.php');
-
-echo "Hello there! It looks like you're logged in";
-
-include(LOGIN_VIEWS_PATH.'footer.php');
--- public_html/second.php
+++ public_html/second.php
@@ -3,10 +3,7 @@
 require_once('../inc/config.php');
 authenticate();
 
+?>
 
-
-include(LOGIN_VIEWS_PATH.'header.php');
-
-echo "This is another page. This is a test to see if you'll get redirected back here after login";
-
-include(LOGIN_VIEWS_PATH.'footer.php');
+<a href="<?= LOGOUT_URL; ?>">Logout</a>
+<p>This is another page. This is a test to see if you'll get redirected back here after login</p>
