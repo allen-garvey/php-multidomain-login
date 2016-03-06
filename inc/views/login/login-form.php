790ed5fcdc22ac7df684ac486658773ca0e3d129
Allen Garvey <allengarvey@hotmail.com>
2016-03-05 21:45:01 -0500

    add bootstrap form-control classes to inputs

--- inc/views/login/login-form.php
+++ inc/views/login/login-form.php
@@ -3,15 +3,15 @@
 	<form method="POST" action="" class="login_form">
 		<div class="form-group">
 			<label for="<?= LOGIN_FORM_USER; ?>">Username</label>
-			<input type="text" placeholder="Username" id="<?= LOGIN_FORM_USER; ?>" name="<?= LOGIN_FORM_USER; ?>" autofocus="autofocus" />
+			<input class="form-control" type="text" placeholder="Username" id="<?= LOGIN_FORM_USER; ?>" name="<?= LOGIN_FORM_USER; ?>" autofocus="autofocus" />
 		</div>
 		<div class="form-group">
 			<label for="<?= LOGIN_FORM_PASS; ?>">Password</label>
-			<input type="password" placeholder="Password" id="<?= LOGIN_FORM_PASS; ?>" name="<?= LOGIN_FORM_PASS; ?>" />
+			<input class="form-control" type="password" placeholder="Password" id="<?= LOGIN_FORM_PASS; ?>" name="<?= LOGIN_FORM_PASS; ?>" />
 		</div>
 		<div class="form-group captcha">	
 			<label for="<?= LOGIN_FORM_CAPTCHA; ?>"><?= $captcha_text; ?></label>
-			<input type="number" placeholder="?" id="<?= LOGIN_FORM_CAPTCHA; ?>" name="<?= LOGIN_FORM_CAPTCHA; ?>" />
+			<input class="form-control" type="number" placeholder="?" id="<?= LOGIN_FORM_CAPTCHA; ?>" name="<?= LOGIN_FORM_CAPTCHA; ?>" />
 			<button type="submit" class="btn btn-primary">Login</button>
 		</div>
 	</form>
