78505be045c3730e2c0447d5bfb7926b8f90d33f
Allen Garvey <allengarvey@hotmail.com>
2016-03-05 21:37:35 -0500

    add full bootstrap and default theme css import, add alert box markup and move container from login form

--- inc/views/login/login-page.php
+++ inc/views/login/login-page.php
@@ -2,6 +2,9 @@
 <html>
 <head>
 	<title><?= APP_NAME ?> - Login</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 	<style>
 		*{
 			box-sizing: border-box
@@ -14,9 +17,12 @@
 			text-align: center;
 			margin: 0;
 		}
+		.alert{
+			text-align: center;
+		}
 		.container{
 			max-width: 40em;
-			margin: 12em auto 0;
+			margin: 6em auto;
 		}
 		.login_form{
 			background-color: #ccc;
@@ -75,146 +81,22 @@
 		.legal{
 			text-align: center;
 		}
-		<?php //bootstrap buttons ?>
-		.btn {
-		  display: inline-block;
-		  padding: 6px 12px;
-		  margin-bottom: 0;
-		  font-size: 14px;
-		  font-weight: normal;
-		  line-height: 1.42857143;
-		  text-align: center;
-		  white-space: nowrap;
-		  vertical-align: middle;
-		  -ms-touch-action: manipulation;
-		      touch-action: manipulation;
-		  cursor: pointer;
-		  -webkit-user-select: none;
-		     -moz-user-select: none;
-		      -ms-user-select: none;
-		          user-select: none;
-		  background-image: none;
-		  border: 1px solid transparent;
-		  border-radius: 4px;
-		}
-		.btn:focus,
-		.btn:active:focus,
-		.btn.active:focus,
-		.btn.focus,
-		.btn:active.focus,
-		.btn.active.focus {
-		  outline: thin dotted;
-		  outline: 5px auto -webkit-focus-ring-color;
-		  outline-offset: -2px;
-		}
-		.btn:hover,
-		.btn:focus,
-		.btn.focus {
-		  color: #333;
-		  text-decoration: none;
-		}
-		.btn:active,
-		.btn.active {
-		  background-image: none;
-		  outline: 0;
-		  -webkit-box-shadow: inset 0 3px 5px rgba(0, 0, 0, .125);
-		          box-shadow: inset 0 3px 5px rgba(0, 0, 0, .125);
-		}
-
-		.btn-primary {
-		  color: #fff;
-		  background-color: #337ab7;
-		  border-color: #2e6da4;
-		}
-		.btn-primary:focus,
-		.btn-primary.focus {
-		  color: #fff;
-		  background-color: #286090;
-		  border-color: #122b40;
-		}
-		.btn-primary:hover {
-		  color: #fff;
-		  background-color: #286090;
-		  border-color: #204d74;
-		}
-		.btn-primary:active,
-		.btn-primary.active,
-		.open > .dropdown-toggle.btn-primary {
-		  color: #fff;
-		  background-color: #286090;
-		  border-color: #204d74;
-		}
-		.btn-primary:active:hover,
-		.btn-primary.active:hover,
-		.open > .dropdown-toggle.btn-primary:hover,
-		.btn-primary:active:focus,
-		.btn-primary.active:focus,
-		.open > .dropdown-toggle.btn-primary:focus,
-		.btn-primary:active.focus,
-		.btn-primary.active.focus,
-		.open > .dropdown-toggle.btn-primary.focus {
-		  color: #fff;
-		  background-color: #204d74;
-		  border-color: #122b40;
-		}
-		.btn-primary:active,
-		.btn-primary.active,
-		.open > .dropdown-toggle.btn-primary {
-		  background-image: none;
-		}
-		<?php //bootstrap default theme buttons ?>
-		.btn-default,
-		.btn-primary,
-		.btn-success,
-		.btn-info,
-		.btn-warning,
-		.btn-danger {
-		  text-shadow: 0 -1px 0 rgba(0, 0, 0, .2);
-		  -webkit-box-shadow: inset 0 1px 0 rgba(255, 255, 255, .15), 0 1px 1px rgba(0, 0, 0, .075);
-		          box-shadow: inset 0 1px 0 rgba(255, 255, 255, .15), 0 1px 1px rgba(0, 0, 0, .075);
-		}
-		.btn-default:active,
-		.btn-primary:active,
-		.btn-success:active,
-		.btn-info:active,
-		.btn-warning:active,
-		.btn-danger:active,
-		.btn-default.active,
-		.btn-primary.active,
-		.btn-success.active,
-		.btn-info.active,
-		.btn-warning.active,
-		.btn-danger.active {
-		  -webkit-box-shadow: inset 0 3px 5px rgba(0, 0, 0, .125);
-		          box-shadow: inset 0 3px 5px rgba(0, 0, 0, .125);
-		}
-		.btn:active,
-		.btn.active {
-		  background-image: none;
-		}
-		.btn-primary {
-		  background-image: -webkit-linear-gradient(top, #337ab7 0%, #265a88 100%);
-		  background-image:      -o-linear-gradient(top, #337ab7 0%, #265a88 100%);
-		  background-image: -webkit-gradient(linear, left top, left bottom, from(#337ab7), to(#265a88));
-		  background-image:         linear-gradient(to bottom, #337ab7 0%, #265a88 100%);
-		  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#ff337ab7', endColorstr='#ff265a88', GradientType=0);
-		  filter: progid:DXImageTransform.Microsoft.gradient(enabled = false);
-		  background-repeat: repeat-x;
-		  border-color: #245580;
-		}
-		.btn-primary:hover,
-		.btn-primary:focus {
-		  background-color: #265a88;
-		  background-position: 0 -15px;
-		}
-		.btn-primary:active,
-		.btn-primary.active {
-		  background-color: #265a88;
-		  border-color: #245580;
-		}
+		
 	</style>
 </head>
 <body>
-	<?php include(LOGIN_VIEWS_PATH.'login-form.php'); ?>
+	<div class="container">
+		<?php if(!empty($flash)): ?>
+			<div class="alert alert-<?= $flash['class']; ?>" role="alert">
+				<?= $flash['message']; ?>
+			</div>
+		<?php endif; ?>
+		<?php include(LOGIN_VIEWS_PATH.'login-form.php'); ?>
+	</div>
 </body>
 </html>
+
+
+
+
+
