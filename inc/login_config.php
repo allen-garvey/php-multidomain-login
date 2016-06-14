21f44cd8177db271a1f82bfb20c1234a90083f30
Allen Garvey <allengarvey@hotmail.com>
2016-06-13 23:29:16 -0400

    start moving login constants into namespaced classes

--- inc/login_config.php
+++ inc/login_config.php
@@ -1,5 +1,33 @@
 <?php 
 
+class AuthController{
+	//paths
+	const VIEWS_PATH = VIEWS_PATH.'login/';
+
+	//login form names
+	const LOGIN_FORM_USER = 'username';
+	const LOGIN_FORM_PASS = 'password';
+	const LOGIN_FORM_CAPTCHA = 'captcha';
+
+	//session keys
+	const SESSION_AUTH_KEY = 'session_username_key';
+	const SESSION_REDIRECT_URL_KEY = 'session_redirect_url_key';
+
+	//urls
+	const LOGGED_IN_HOME_URL = BASE_URL.'index.php';
+	const LOGIN_URL = BASE_URL.'login';
+	const LOGOUT_URL = LOGIN_URL.'/?logout=true';
+
+}
+
+class CaptchaController{
+	const CAPTCHA_NUMBER_MIN = 1;
+	const CAPTCHA_NUMBER_MAX = 10;
+
+	const SESSION_CAPTCHA_ANSWER_KEY = 'session_captcha_answer_key';
+}
+
+
 //paths
 define('LOGIN_VIEWS_PATH', VIEWS_PATH.'login/');
 
