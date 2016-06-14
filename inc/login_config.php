a843e164c4d4b9fbb4f41d3dc01b1088c1410c58
Allen Garvey <allengarvey@hotmail.com>
2016-06-14 17:34:07 -0400

    move login functions into appropriate controllers

--- inc/login_config.php
+++ inc/login_config.php
@@ -18,6 +18,53 @@ class AuthController{
 	const LOGIN_URL = BASE_URL.'login';
 	const LOGOUT_URL = LOGIN_URL.'/?logout=true';
 
+	public static function destroy_session(){
+		//remove PHPSESSID from browser
+		if ( isset( $_COOKIE[session_name()] ) ){
+			setcookie( session_name(), '', time()-3600, '/' );
+		}
+		//clear session from globals
+		$_SESSION = array();
+		//clear session from disk
+		session_destroy();
+	}
+
+	public static function is_authenticated(string $username=null, string $password=null): bool {
+		return !empty($username) && !empty($password) && AuthController::authenticate_user($username, $password);
+	}
+
+	public static function authenticate_user(string $username, string $password) : bool {
+		return $username === MASTER_USER && password_verify($password, MASTER_PASSWORD_HASH);
+	}
+
+	/*
+	* If redirect= true, on failed authentication will redirect to login page
+	* use redirect=false for resources, so that the request will simply fail
+	*/
+	public static function authenticate(bool $redirect = true){
+		if(REQUIRE_LOGIN === false){
+			return;
+		}
+		session_start();
+		if(empty($_SESSION) || empty($_SESSION[AuthController::SESSION_AUTH_KEY])){
+			if($redirect){
+				$_SESSION[AuthController::SESSION_REDIRECT_URL_KEY] = $_SERVER['REQUEST_URI'];
+				http_response_code(400);
+				header('Location: '.AuthController::LOGIN_URL);
+			}
+			else{
+				http_response_code(404);
+			}
+			die();
+		}
+	}
+	/*
+	* Saves that the user is logged in in session so other subdomains can access it
+	*/
+	public static function save_auth(string $username){
+		$_SESSION[AuthController::SESSION_AUTH_KEY] = $username;
+	}
+
 }
 
 class CaptchaController{
@@ -25,6 +72,42 @@ class CaptchaController{
 	const CAPTCHA_NUMBER_MAX = 10;
 
 	const SESSION_CAPTCHA_ANSWER_KEY = 'session_captcha_answer_key';
+
+	public static function generate_captcha() : string {
+	
+		$num1 = random_int(CaptchaController::CAPTCHA_NUMBER_MIN, CaptchaController::CAPTCHA_NUMBER_MAX);
+		$num2 = random_int(CaptchaController::CAPTCHA_NUMBER_MIN, CaptchaController::CAPTCHA_NUMBER_MAX);
+
+		switch(random_int(0, 3)){
+			case 0:
+				$max = max($num1, $num2);
+				$min = min($num1, $num2);
+				$answer = $max - $min;
+				$question = "$max - $min =";
+				break;
+			case 1:
+				$answer = $num1;
+				$squared = $num1 * $num1;
+				$question = "&radic;$squared =";
+				break;
+			case 2:
+				$answer = $num1 * $num2;
+				$question = "$num1 &times; $num2 =";
+				break;
+			
+			default:
+				$answer = $num1 + $num2;
+				$question = "$num1 + $num2 =";
+				break;
+		}
+
+		$_SESSION[CaptchaController::SESSION_CAPTCHA_ANSWER_KEY] = $answer;
+
+		return $question;
+	}
+	public static function validate_captcha($answer) : bool {
+		return isset($answer) && (int) $answer === $_SESSION[CaptchaController::SESSION_CAPTCHA_ANSWER_KEY];
+	}
 }
 
 
